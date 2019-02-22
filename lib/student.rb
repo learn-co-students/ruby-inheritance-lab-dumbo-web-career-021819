1a4c69e3d7e700ad94b141d5a5a20f0f518caf79
brianryu <s.brianryu@gmail.com>
2019-02-22 16:19:05 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def new
+    @knowledge = []
+  end
+
+  def learn(str)
+    @knowledge << str
+  end
+
+  def knowledge
+    return @knowledge
+  end
 
 end
