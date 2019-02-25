23645e670d417062771c19466e89cb2e1b14d1bc
campbelllssoup <matjocampbell@gmail.com>
2019-02-25 09:04:55 -0500

    Creates the Student, Teacher, and User classes

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(new_knowledge)
+    self.knowledge.push(new_knowledge)
+  end
+
 
 end
